564e1ab3894b896cb6082cf8051a922fb0910743
Henry Estela <henryestela@gmail.com>
2015-05-12 08:56:06 -0700

    Created fns for creating predefined comms. ex. MPI_COMM_WORLD can be used by calling comm_world(). It is a function and not an object because of the type of an MPI_Comm object as *mut Struct_ompi_communicator_t.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+// TODO: figure out how to repesent error conditions.
 extern crate libc;
 
 use std::env::Args;
@@ -7,17 +8,30 @@ use libc::{c_longlong, c_void, c_uint, c_double};
 pub mod bindings;
 use bindings::*;
 
-// Aliases for MPI_* types
-pub type CommWorld = ompi_mpi_comm_world;
+pub fn comm_new() -> MPI_Comm {
+  unsafe { &mut ompi_communicator_t }
+}
 
-// TODO: figure out how to repesent error conditions.
+pub fn comm_world() -> MPI_Comm {
+  unsafe { &mut ompi_mpi_comm_world }
+}
+
+pub fn comm_self() -> MPI_Comm {
+  unsafe { &mut ompi_mpi_comm_self }
+}
+
+pub fn comm_null() -> MPI_Comm {
+  unsafe { &mut ompi_mpi_comm_null }
+}
+
 /*
 pub fn init(argc: usize, argv: Vec<Args>) -> () {
   unsafe { ffi::MPI_Init(&argc as *mut c_int, argv) } //TODO: howdoi?
 }
 */
 
-/// Calls MPI_Init with null parameters, temporary function
+/// Calls MPI_Init with null parameters.
+/// As of MPI-2, MPI_Init will accept NULL as input parameters.
 pub fn init_null() -> i32 {
   let argc = 0 as *mut c_int;
   let argv = 0 as *mut *mut *mut c_char;
@@ -28,6 +42,8 @@ pub fn finalize() -> i32 {
   unsafe { MPI_Finalize() }
 }
 
+/// Returns the rank of the process in the communicator.
+/// Doesnt "return" rank as a pointer link in C.
 // TODO: Handle errors, dont just throw the error away
 pub fn comm_rank(comm: MPI_Comm) -> i32 {
   let mut rank = -1;
